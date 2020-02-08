c55fd68085f2068602a6559667f44e70dbf85c80
Johar M. Ashfaque <39216339+UKVeteran@users.noreply.github.com>
2020-02-08 16:09:26 +0000

    Add files via upload

---
+++ Wifi-Pass-Code/WifiPass/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace WifiPass
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.metroButtonGet = new MetroFramework.Controls.MetroButton();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView1.FullRowSelect = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(23, 132);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(826, 424);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Wifi Name";
+            this.columnHeader1.Width = 303;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Wifi Password";
+            this.columnHeader2.Width = 513;
+            // 
+            // metroButtonGet
+            // 
+            this.metroButtonGet.Location = new System.Drawing.Point(714, 94);
+            this.metroButtonGet.Name = "metroButtonGet";
+            this.metroButtonGet.Size = new System.Drawing.Size(135, 23);
+            this.metroButtonGet.TabIndex = 4;
+            this.metroButtonGet.Text = "Get Passwords";
+            this.metroButtonGet.Click += new System.EventHandler(this.metroButtonGet_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.BackColor = System.Drawing.Color.White;
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2,
+            this.toolStripStatusLabel3});
+            this.statusStrip1.Location = new System.Drawing.Point(20, 574);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(816, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(170, 17);
+            this.toolStripStatusLabel1.Text = "Developer: Hasan Aboul Hasan";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(10, 17);
+            this.toolStripStatusLabel2.Text = "|";
+            // 
+            // toolStripStatusLabel3
+            // 
+            this.toolStripStatusLabel3.IsLink = true;
+            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(63, 17);
+            this.toolStripStatusLabel3.Text = "H-educate";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(856, 616);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.metroButtonGet);
+            this.Controls.Add(this.listView1);
+            this.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Resizable = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Local Wifi Passwords";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private MetroFramework.Controls.MetroButton metroButtonGet;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+    }
+}
+
---
+++ Wifi-Pass-Code/WifiPass/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace WifiPass
+{
+    //Credits, The main project idea is located on https://laptrinhvb.net/
+
+
+
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int wifiCount = 0;
+        int Wifi_count_names = 0;
+  
+        private string GetWifiNetworks()
+        {
+            //execute the netsh command using process class
+            Process processWifi = new Process();
+            processWifi.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            processWifi.StartInfo.FileName = "netsh";
+            processWifi.StartInfo.Arguments = "wlan show profile";
+            
+            processWifi.StartInfo.UseShellExecute = false;
+            processWifi.StartInfo.RedirectStandardError = true;
+            processWifi.StartInfo.RedirectStandardInput = true;
+            processWifi.StartInfo.RedirectStandardOutput = true;
+            processWifi.StartInfo.CreateNoWindow = true;
+            processWifi.Start();
+
+            string output = processWifi.StandardOutput.ReadToEnd();
+
+            processWifi.WaitForExit();
+            return output;
+        }
+
+        private string ReadPassword(string Wifi_Name)
+        {
+
+            string argument = "wlan show profile name=\"" + Wifi_Name + "\" key=clear";
+            Process processWifi = new Process();
+            processWifi.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            processWifi.StartInfo.FileName = "netsh";
+            processWifi.StartInfo.Arguments = argument;
+
+
+            processWifi.StartInfo.UseShellExecute = false;
+            processWifi.StartInfo.RedirectStandardError = true;
+            processWifi.StartInfo.RedirectStandardInput = true;
+            processWifi.StartInfo.RedirectStandardOutput = true;
+            processWifi.StartInfo.CreateNoWindow = true;
+            processWifi.Start();
+
+            string output = processWifi.StandardOutput.ReadToEnd();
+
+            processWifi.WaitForExit();
+            return output;
+        }
+
+        private string GetWifiPassword(string Wifi_Name)
+        {
+            string get_password = ReadPassword(Wifi_Name);
+            using (StringReader reader = new StringReader(get_password))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    Regex regex2 = new Regex(@"Key Content * : (?<after>.*)");
+                    Match match2 = regex2.Match(line);
+
+                    if (match2.Success)
+                    {
+                        string current_password = match2.Groups["after"].Value;
+                        return current_password;
+                    }
+                }
+            }
+            return "Open Network - NO PASSWORD";
+        }
+    
+        // main Method 
+        private void get_Wifi_passwords()
+        {
+            string WifiNetworks = GetWifiNetworks();
+            using (StringReader reader = new StringReader(WifiNetworks))
+            {
+
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    wifiCount++;
+                    Regex regex1 = new Regex(@"All User Profile * : (?<after>.*)");
+                    Match match1 = regex1.Match(line);
+
+                    if (match1.Success)
+                    {
+                        Wifi_count_names++;
+                        string Wifi_name = match1.Groups["after"].Value;
+                        string Wifi_password = GetWifiPassword(Wifi_name);
+                        listView1.Items.Add(Wifi_name).SubItems.Add(Wifi_password);
+                    }
+                }
+            }
+
+        
+        }
+        
+        private void metroButtonGet_Click(object sender, EventArgs e)
+        {
+            //clear the Listview befire starting
+            listView1.Items.Clear();
+            metroButtonGet.Enabled = false;
+            get_Wifi_passwords();
+            metroButtonGet.Enabled = true;
+        }
+
+      
+    }
+}
