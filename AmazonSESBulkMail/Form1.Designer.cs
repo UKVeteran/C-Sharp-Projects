89937b8d713600200c8630e7c520cb1b2cd0a831
Johar M. Ashfaque <39216339+UKVeteran@users.noreply.github.com>
2020-02-09 12:04:34 +0000

    Add files via upload

---
+++ AmazonSESBulkMail/Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace AmazonSESBulkMail
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.pictureBoxLoading = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxSubject = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxHTMLBody = new System.Windows.Forms.TextBox();
+            this.buttonTxt = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBoxEMails = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label12 = new System.Windows.Forms.Label();
+            this.textBoxUSER = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBoxPORT = new System.Windows.Forms.TextBox();
+            this.textBoxSenderEmail = new System.Windows.Forms.TextBox();
+            this.comboBoxRegion = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLoading)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSend.Location = new System.Drawing.Point(781, 350);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(184, 42);
+            this.buttonSend.TabIndex = 0;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBoxLoading
+            // 
+            this.pictureBoxLoading.Image = global::AmazonSESBulkMail.Properties.Resources.giphy;
+            this.pictureBoxLoading.Location = new System.Drawing.Point(781, 398);
+            this.pictureBoxLoading.Name = "pictureBoxLoading";
+            this.pictureBoxLoading.Size = new System.Drawing.Size(184, 63);
+            this.pictureBoxLoading.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxLoading.TabIndex = 41;
+            this.pictureBoxLoading.TabStop = false;
+            this.pictureBoxLoading.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(313, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 40;
+            this.label3.Text = "Subject: ";
+            // 
+            // textBoxSubject
+            // 
+            this.textBoxSubject.Location = new System.Drawing.Point(395, 51);
+            this.textBoxSubject.Name = "textBoxSubject";
+            this.textBoxSubject.Size = new System.Drawing.Size(282, 20);
+            this.textBoxSubject.TabIndex = 39;
+            this.textBoxSubject.Text = "C# Test Message From AWS SES API";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(313, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 13);
+            this.label2.TabIndex = 38;
+            this.label2.Text = "Email body (HTML)";
+            // 
+            // textBoxHTMLBody
+            // 
+            this.textBoxHTMLBody.Location = new System.Drawing.Point(316, 99);
+            this.textBoxHTMLBody.Multiline = true;
+            this.textBoxHTMLBody.Name = "textBoxHTMLBody";
+            this.textBoxHTMLBody.Size = new System.Drawing.Size(361, 362);
+            this.textBoxHTMLBody.TabIndex = 37;
+            this.textBoxHTMLBody.Text = "<html>\r\n<head></head>\r\n<body>\r\n  <h1>Amazon SES API Test C#)</h1>\r\n  <p>This emai" +
+    "l was sent with C# Bulk Email Sender\r\n      using Amazon SES API and .NET SDK\r\n " +
+    "   <p>\r\n</body>\r\n</html>";
+            // 
+            // buttonTxt
+            // 
+            this.buttonTxt.Location = new System.Drawing.Point(135, 16);
+            this.buttonTxt.Name = "buttonTxt";
+            this.buttonTxt.Size = new System.Drawing.Size(103, 23);
+            this.buttonTxt.TabIndex = 36;
+            this.buttonTxt.Text = "Import From Text";
+            this.buttonTxt.UseVisualStyleBackColor = true;
+            this.buttonTxt.Click += new System.EventHandler(this.buttonTxt_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 18);
+            this.label1.TabIndex = 34;
+            this.label1.Text = "Emails List";
+            // 
+            // listBoxEMails
+            // 
+            this.listBoxEMails.FormattingEnabled = true;
+            this.listBoxEMails.Location = new System.Drawing.Point(30, 54);
+            this.listBoxEMails.Name = "listBoxEMails";
+            this.listBoxEMails.Size = new System.Drawing.Size(262, 407);
+            this.listBoxEMails.TabIndex = 33;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(313, 17);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 18);
+            this.label4.TabIndex = 42;
+            this.label4.Text = "Message";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(714, 228);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(47, 13);
+            this.label12.TabIndex = 50;
+            this.label12.Text = "Region: ";
+            // 
+            // textBoxUSER
+            // 
+            this.textBoxUSER.Location = new System.Drawing.Point(791, 170);
+            this.textBoxUSER.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBoxUSER.Name = "textBoxUSER";
+            this.textBoxUSER.PasswordChar = '*';
+            this.textBoxUSER.Size = new System.Drawing.Size(174, 20);
+            this.textBoxUSER.TabIndex = 48;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(713, 173);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(66, 13);
+            this.label11.TabIndex = 47;
+            this.label11.Text = "App Secret: ";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(713, 137);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(48, 13);
+            this.label10.TabIndex = 46;
+            this.label10.Text = "API Key:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(713, 101);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(72, 13);
+            this.label9.TabIndex = 45;
+            this.label9.Text = "Sender Email:";
+            // 
+            // textBoxPORT
+            // 
+            this.textBoxPORT.Location = new System.Drawing.Point(791, 134);
+            this.textBoxPORT.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBoxPORT.Name = "textBoxPORT";
+            this.textBoxPORT.Size = new System.Drawing.Size(174, 20);
+            this.textBoxPORT.TabIndex = 44;
+            // 
+            // textBoxSenderEmail
+            // 
+            this.textBoxSenderEmail.Location = new System.Drawing.Point(791, 98);
+            this.textBoxSenderEmail.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBoxSenderEmail.Name = "textBoxSenderEmail";
+            this.textBoxSenderEmail.Size = new System.Drawing.Size(174, 20);
+            this.textBoxSenderEmail.TabIndex = 43;
+            // 
+            // comboBoxRegion
+            // 
+            this.comboBoxRegion.FormattingEnabled = true;
+            this.comboBoxRegion.Items.AddRange(new object[] {
+            "US WEST (Oregon)",
+            "US East (N. Virginia)",
+            "EU (Ireland)"});
+            this.comboBoxRegion.Location = new System.Drawing.Point(792, 225);
+            this.comboBoxRegion.Name = "comboBoxRegion";
+            this.comboBoxRegion.Size = new System.Drawing.Size(174, 21);
+            this.comboBoxRegion.TabIndex = 51;
+            this.comboBoxRegion.SelectedIndexChanged += new System.EventHandler(this.comboBoxRegion_SelectedIndexChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(713, 17);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(107, 18);
+            this.label5.TabIndex = 52;
+            this.label5.Text = "SES Settings";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(791, 196);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(50, 13);
+            this.linkLabel1.TabIndex = 53;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Get Keys";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1025, 480);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBoxRegion);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.textBoxUSER);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBoxPORT);
+            this.Controls.Add(this.textBoxSenderEmail);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBoxLoading);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxSubject);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxHTMLBody);
+            this.Controls.Add(this.buttonTxt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBoxEMails);
+            this.Controls.Add(this.buttonSend);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "C# Bulk Sender With Amazon SES API";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLoading)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.PictureBox pictureBoxLoading;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxSubject;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxHTMLBody;
+        private System.Windows.Forms.Button buttonTxt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBoxEMails;
+        private System.Windows.Forms.Label label4;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox textBoxUSER;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBoxPORT;
+        private System.Windows.Forms.TextBox textBoxSenderEmail;
+        private System.Windows.Forms.ComboBox comboBoxRegion;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ AmazonSESBulkMail/Form1.cs
@@ -0,0 +1,204 @@
+﻿using Amazon;
+using Amazon.SimpleEmail;
+using Amazon.SimpleEmail.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AmazonSESBulkMail
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+      
+
+        //Form Load 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBoxRegion.SelectedIndex = 0; //Select the first Index by Defualt US WEST (Oregon)
+            SelectAWSRegion = RegionEndpoint.USWest2;
+        }
+
+        // SES API KEYs LINK
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://console.aws.amazon.com/iam/home?#security_credential");
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+
+            buttonSend.Enabled = false; //avoid multiple clicks
+            pictureBoxLoading.Visible = true; //Show loading image (just for design)
+            backgroundWorker1.RunWorkerAsync(); //run the code in background for better user experience
+
+        }
+
+        //Import Email list from Text File
+        private void buttonTxt_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Multiselect = false;
+
+            DialogResult dr = openFile.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                // Read the file and display it line by line.  
+                System.IO.StreamReader file =
+                    new System.IO.StreamReader(@openFile.FileName);
+                string line;
+                while ((line = file.ReadLine()) != null)
+                {
+
+                    // add to listbox
+                    listBoxEMails.Items.Add(line);
+
+                }
+
+                file.Close();
+
+
+            }
+        }
+
+        private RegionEndpoint SelectAWSRegion = RegionEndpoint.USWest2;
+        private void comboBoxRegion_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            // set the Region according to combobox selection
+            if (comboBoxRegion.SelectedIndex == 0)
+            {
+                SelectAWSRegion = RegionEndpoint.USWest2;
+            }
+
+            if (comboBoxRegion.SelectedIndex == 1)
+            {
+                SelectAWSRegion = RegionEndpoint.USEast1;
+            }
+
+            if (comboBoxRegion.SelectedIndex == 2)
+            {
+                SelectAWSRegion = RegionEndpoint.USWest1;
+            }
+
+
+
+        }
+
+        //Main Sending code using the background worker
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+            Amazon.Runtime.AWSCredentials newcred = new Amazon.Runtime.BasicAWSCredentials(textBoxPORT.Text, textBoxUSER.Text);
+
+            using (var client = new AmazonSimpleEmailServiceClient(newcred, SelectAWSRegion))
+            {
+                var sendRequest = new SendEmailRequest
+                {
+                    Source = textBoxSenderEmail.Text,
+                    Destination = new Destination
+                    {
+
+                        ToAddresses = listBoxEMails.Items.Cast<String>().ToList()
+                        // new List<string> { listBoxEMails.Items.Cast<String>().ToList() }
+                    },
+                    Message = new Amazon.SimpleEmail.Model.Message
+                    {
+                        Subject = new Content(textBoxSubject.Text),
+                        Body = new Body
+                        {
+                            
+                            Html = new Content
+                            {
+                                Charset = "UTF-8",
+                                Data = textBoxHTMLBody.Text
+                            },
+                          
+                        }
+                    },
+
+                };
+                try
+                {
+                    var response = client.SendEmail(sendRequest);
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error message: " + ex.Message);
+                }
+            }
+
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            buttonSend.Enabled = true; 
+            pictureBoxLoading.Visible = false;
+            MessageBox.Show("Finished!");
+        }
+
+
+
+        
+
+        //AWS SES Single Send Method for customzing (not used here in this application)
+        public string SES_SendMessage(string FromEmail, string ToEmail,
+            string subject, string Body, string APIKEY, string APISecret, RegionEndpoint region)
+        {
+            Amazon.Runtime.AWSCredentials newcred = new Amazon.Runtime.BasicAWSCredentials(APIKEY, APISecret);
+
+            using (var client = new AmazonSimpleEmailServiceClient(newcred, region))
+            {
+                var sendRequest = new SendEmailRequest
+                {
+                    Source = FromEmail,
+                    Destination = new Destination
+                    {
+                        ToAddresses =
+                        new List<string> { ToEmail }
+                    },
+                    Message = new Amazon.SimpleEmail.Model.Message
+                    {
+                        Subject = new Content(subject),
+                        Body = new Body
+                        {
+                            Text = new Content
+                            {
+                                Charset = "UTF-8",
+                                Data = Body
+                            }
+                        }
+                    },
+
+                };
+                try
+                {
+                    var response = client.SendEmail(sendRequest);
+                    return "Sent";
+                }
+                catch (Exception ex)
+                {
+
+                    return "Error message: " + ex.Message;
+
+                }
+            }
+        }
+
+     
+    }
+}
